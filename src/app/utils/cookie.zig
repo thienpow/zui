406c5b0a49b99270978d0490d431e9536986067d
Pow <thienpow@gmail.com>
2025-03-12 21:25:57 +0800

    cookie options

--- src/app/utils/cookie.zig
+++ src/app/utils/cookie.zig
@@ -2,37 +2,30 @@ const std = @import("std");
 const jetzig = @import("jetzig");
 
 pub fn set_cookie(request: *jetzig.Request, name: []const u8, value: []const u8) !void {
-    var cookies = try request.cookies();
-
-    var cookie = jetzig.http.Cookies.Cookie{
+    const cookies = try request.cookies();
+    try cookies.put(jetzig.http.Cookies.Cookie{
         .name = name,
         .value = value,
-    };
-
-    cookie.domain = request.global.config_manager.security_config.session.cookie_domain;
-    cookie.path = "/";
-    cookie.http_only = true;
-    cookie.secure = true;
-    cookie.same_site = .strict;
-    cookie.max_age = 60 * 60 * 24 * 90; // 90 days in seconds
-
-    try cookies.put(cookie);
+        .domain = request.global.config_manager.security_config.session.cookie_domain,
+        .path = "/",
+        .http_only = true,
+        .secure = true,
+        .same_site = .strict,
+        .max_age = 60 * 60 * 24 * 90, // 90 days in seconds
+    });
 }
 
 pub fn set_oauth_cookie(request: *jetzig.Request, value: []const u8) !void {
-    var cookies = try request.cookies();
-
-    var cookie = jetzig.http.Cookies.Cookie{
+    const cookies = try request.cookies();
+    try cookies.put(jetzig.http.Cookies.Cookie{
         .name = request.global.config_manager.security_config.oauth.state_cookie_name,
         .value = value,
-    };
+        .domain = request.global.config_manager.security_config.session.cookie_domain,
+        .path = "/",
+        .http_only = true,
+        .secure = true,
+        .same_site = .lax,
+        .max_age = 60 * 5, // 5 minutes in seconds
 
-    cookie.domain = request.global.config_manager.security_config.session.cookie_domain;
-    cookie.path = "/";
-    cookie.http_only = true;
-    cookie.secure = true;
-    cookie.same_site = .lax;
-    cookie.max_age = 60 * 5; // 5 minutes in seconds
-
-    try cookies.put(cookie);
+    });
 }
