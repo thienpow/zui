6ee7e28eb66ecf42064e0d4a7a7fc2f1d2ddbae0
Pow <thienpow@gmail.com>
2025-03-12 21:16:33 +0800

    cookie options test

--- src/app/utils/cookie.zig
+++ src/app/utils/cookie.zig
@@ -9,11 +9,11 @@ pub fn set_cookie(request: *jetzig.Request, name: []const u8, value: []const u8)
         .value = value,
     };
 
-    // cookie.domain = request.global.config_manager.security_config.session.cookie_domain;
-    // cookie.path = "/";
-    // cookie.http_only = true;
-    // cookie.secure = true;
-    // cookie.same_site = .strict;
+    cookie.domain = request.global.config_manager.security_config.session.cookie_domain;
+    cookie.path = "/";
+    cookie.http_only = true;
+    cookie.secure = true;
+    cookie.same_site = .strict;
     cookie.max_age = 60 * 60 * 24 * 90; // 90 days in seconds
 
     try cookies.put(cookie);
@@ -27,10 +27,10 @@ pub fn set_oauth_cookie(request: *jetzig.Request, value: []const u8) !void {
         .value = value,
     };
 
-    // cookie.domain = request.global.config_manager.security_config.session.cookie_domain;
-    // cookie.path = "/";
-    // cookie.http_only = true;
-    // cookie.secure = true;
+    cookie.domain = request.global.config_manager.security_config.session.cookie_domain;
+    cookie.path = "/";
+    cookie.http_only = true;
+    cookie.secure = true;
     cookie.same_site = .lax;
     cookie.max_age = 60 * 5; // 5 minutes in seconds
 
