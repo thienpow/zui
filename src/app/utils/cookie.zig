f9a8d7480809d5bf607455493cca2bd5a8aa3822
Pow <thienpow@gmail.com>
2025-03-12 20:08:07 +0800

    cookie test

--- src/app/utils/cookie.zig
+++ src/app/utils/cookie.zig
@@ -4,7 +4,7 @@ const jetzig = @import("jetzig");
 pub fn set_cookie(request: *jetzig.Request, name: []const u8, value: []const u8) !void {
     var cookies = try request.cookies();
 
-    try cookies.put(.{
+    cookies.put(jetzig.http.Cookies.Cookie{
         .name = name,
         .value = value,
         .domain = request.global.config_manager.security_config.session.cookie_domain,
@@ -13,13 +13,15 @@ pub fn set_cookie(request: *jetzig.Request, name: []const u8, value: []const u8)
         .secure = true,
         .same_site = .strict,
         .max_age = 60 * 60 * 24 * 90, // 90 days in seconds
-    });
+    }) catch |err| {
+        std.log.scoped(.utils).err("[cookie.set_cookie] Failed to set cookie: {any}", .{err});
+    };
 }
 
 pub fn set_oauth_cookie(request: *jetzig.Request, value: []const u8) !void {
     var cookies = try request.cookies();
 
-    try cookies.put(.{
+    cookies.put(jetzig.http.Cookies.Cookie{
         .name = request.global.config_manager.security_config.oauth.state_cookie_name,
         .value = value,
         .domain = request.global.config_manager.security_config.session.cookie_domain,
@@ -28,5 +30,7 @@ pub fn set_oauth_cookie(request: *jetzig.Request, value: []const u8) !void {
         .secure = true,
         .same_site = .lax,
         .max_age = 60 * 5, // 5 minutes in seconds
-    });
+    }) catch |err| {
+        std.log.scoped(.utils).err("[cookie.set_oauth_cookie] Failed to set cookie: {any}", .{err});
+    };
 }
